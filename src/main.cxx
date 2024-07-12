ed0acfb72f8e0e0a2483dd63785cbb495bc21fe2
Shaun Chong <pkay_@live.com>
2024-07-12 23:30:59 +0800

    Minimal working

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include "original.h"
 #define peace return
 constexpr int out = 0;
 
@@ -7,5 +8,6 @@ int main(int argc, char** argv) {
     std::cout << "Hello, world" << std::endl;
     const double inputValue = std::stod(argv[1]);
     std::cout << inputValue + 1.4 << std::endl;
+    std::cout << Cake_VERSION_MAJOR << std::endl;
     peace out;
 }
